4022ee12d984ab44d7239d9a2c4c09c01de090e3
unknown <abeyrathnenaleen@gmail.com>
2026-02-13 21:33:34 +0530

    First Commit

---
+++ assests/app.js
@@ -0,0 +1,18 @@
+        //    <!-- <section class="credits-panel">
+        //         <div class="card-header">
+        //             <h2>Credits</h2>
+        //         </div>
+                
+        //         <div class="credits-body">
+        //             <div class="role-section">
+        //                 <div class="pill-label">Developers</div>
+        //                 <p>Navitha Aken Abeyrathne</p>
+        //                 <p></p>
+        //             </div>
+
+        //             <div class="role-section">
+        //                 <div class="pill-label">Co-developer</div>
+        //                 <p></p>
+        //             </div>
+        //         </div>
+        //     </section> -->
